208d4e11f233374fedfeb55ef1837927a3e72ddd
Bogdan Velicu <bboodd24@gmail.com>
2024-03-28 13:10:48 +0200

    etapa 3

--- index.js
+++ index.js
@@ -1,18 +1,53 @@
 const express = require("express");
-const fs = require("fs");
+const fs= require('fs');
 // const path=require('path');
 // const sharp=require('sharp');
 // const sass=require('sass');
-// const ejs=require('ejs');
+const ejs=require('ejs');
 
-app = express();
+
+app= express();
 console.log("Folder proiect", __dirname);
 console.log("Cale fisier", __filename);
 console.log("Director de lucru", process.cwd());
 
-app.set("view engine", "ejs");
+app.set("view engine","ejs");
 
-app.use("/resurse", express.static(__dirname + "/resurse"));
+app.use("/resurse", express.static(__dirname+"/resurse"));
+
+app.get(["/","/index","/home"], function(req, res){
+    //res.sendFile(__dirname+"/index.html");
+    res.render("pagini/index");
+});
+
+app.get("/*",function(req,res){
+    console.log(req.url);
+    res.send("pagini"+req.url), function(err, rezHtml){
+    console.log("Pagina",rezHtml);
+    console.log("Eroare",rezHtml);
+    res.send(rezHtml);
+    } 
+})
+
+app.get("/cerere", function(req, res){
+    res.send("<b>Hello</n> <span style='color:red'>World</span>");
+});
+
+app.get("/data", function(req, res, next){
+    res.write("Data: ");
+    next();
+});
+
+app.get("/data", function(req, res){
+    res.write(""+new Date());
+    res.end();
+});
+
+app.get("/suma/:a/:b", function(req, res){
+    var suma=parseInt(req.params.a)+parseInt(req.params.b);
+    res.send(""+suma);
+});
+
 
 app.listen(8080);
 console.log("Serverul a pornit");
