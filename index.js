37de73d1a3faa4ffd196e91ab1029ca4ab66a151
Bogdan Velicu <bboodd24@gmail.com>
2024-06-15 20:33:08 +0300

    etapa 4 + etapa 5

--- index.js
+++ index.js
@@ -1,53 +1,113 @@
 const express = require("express");
-const fs= require('fs');
-// const path=require('path');
-// const sharp=require('sharp');
-// const sass=require('sass');
-const ejs=require('ejs');
+const fs = require("fs");
+const path = require("path");
+const sharp = require("sharp");
+const ejs = require("ejs");
 
-
-app= express();
+app = express();
 console.log("Folder proiect", __dirname);
 console.log("Cale fisier", __filename);
 console.log("Director de lucru", process.cwd());
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
-app.use("/resurse", express.static(__dirname+"/resurse"));
+app.use("/resurse", express.static(__dirname + "/resurse"));
 
-app.get(["/","/index","/home"], function(req, res){
-    //res.sendFile(__dirname+"/index.html");
-    res.render("pagini/index");
+// Galerie
+const galleryJson = require("./galerie.json");
+
+const getCurrentHour = () => {
+  const now = new Date();
+  return now.getHours();
+};
+
+const filterImagesByTime = (images) => {
+  const currentHour = getCurrentHour();
+  return images.filter((image) => {
+    return image.intervale_ore.some(
+      ([start, end]) => currentHour >= start && currentHour <= end,
+    );
+  });
+};
+
+const prepareImages = (images) => {
+  return images.map((image, index) => {
+    return {
+      ...image,
+      index: (index + 1).toString().padStart(2, "0"),
+      url: path.join(galleryJson.cale_galerie, image.cale_relativa),
+    };
+  });
+};
+
+const resizeImage = async (imagePath, width, outputPath) => {
+  try {
+    await sharp(imagePath).resize({ width }).toFile(outputPath);
+  } catch (error) {
+    console.error("Error resizing image:", error);
+  }
+};
+
+galleryJson.imagini.forEach((image) => {
+  const fullImagePath = path.join(
+    __dirname,
+    galleryJson.cale_galerie,
+    image.cale_relativa,
+  );
+
+  // Create small version
+  const smallImagePath = fullImagePath.replace(".jpg", `-small.jpg`);
+  if (!fs.existsSync(smallImagePath)) {
+    resizeImage(fullImagePath, 300, smallImagePath);
+  }
+
+  // Create medium version
+  // const mediumImagePath = fullImagePath.replace(".jpg", `-medium.jpg`);
+  // if (!fs.existsSync(mediumImagePath)) {
+  //   resizeImage(fullImagePath, 300, mediumImagePath);
+  // }
+});
+// End Galerie
+
+app.get(["/", "/index", "/home"], function (req, res) {
+  const images = filterImagesByTime(galleryJson.imagini);
+  const preparedImages = prepareImages(images);
+  res.render("pagini/index", { images: preparedImages });
 });
 
-app.get("/*",function(req,res){
-    console.log(req.url);
-    res.send("pagini"+req.url), function(err, rezHtml){
-    console.log("Pagina",rezHtml);
-    console.log("Eroare",rezHtml);
-    res.send(rezHtml);
-    }
-})
-
-app.get("/cerere", function(req, res){
-    res.send("<b>Hello</n> <span style='color:red'>World</span>");
+app.get("/galerie", function (req, res) {
+  const images = prepareImages(galleryJson.imagini);
+  const preparedImages = prepareImages(images);
+  res.render("pagini/galerie_pagina", { images: preparedImages });
 });
 
-app.get("/data", function(req, res, next){
-    res.write("Data: ");
-    next();
+app.get("/api/galerie_json", function (req, res) {
+  res.json(galleryJson);
 });
 
-app.get("/data", function(req, res){
-    res.write(""+new Date());
-    res.end();
+app.get("/cerere", function (req, res) {
+  res.send("<b>Hello</n> <span style='color:red'>World</span>");
 });
 
-app.get("/suma/:a/:b", function(req, res){
-    var suma=parseInt(req.params.a)+parseInt(req.params.b);
-    res.send(""+suma);
+app.get("/data", function (req, res) {
+  res.write("" + new Date());
+  res.end();
 });
 
+app.get("/suma/:a/:b", function (req, res) {
+  var suma = parseInt(req.params.a) + parseInt(req.params.b);
+  res.send("" + suma);
+});
+
+app.get("/*", function (req, res) {
+  console.log(req.url);
+  res.send("pagini" + req.url),
+    function (err, rezHtml) {
+      console.log("Pagina", rezHtml);
+      console.log("Eroare", rezHtml);
+      res.send(rezHtml);
+    };
+});
 
 app.listen(8080);
 console.log("Serverul a pornit");
---
+++ resurse/js/galerie_dinamica.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("/api/galerie_json")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const images = data.imagini;
+      const gallery = document.querySelector(".dynamic-gallery");
+
+      // Random number of images, divisible by 3, less than 16
+      let numImages;
+      do {
+        numImages = Math.floor(Math.random() * 15) + 1;
+      } while (numImages % 3 !== 0 || numImages >= 16);
+
+      // Random offset
+      const offset = Math.floor(Math.random() * (images.length - numImages));
+
+      const selectedImages = images.slice(offset, offset + numImages);
+
+      const galleryInner = document.createElement("div");
+      galleryInner.className = "dynamic-gallery-inner";
+
+      for (let i = 0; i < selectedImages.length; i++) {
+        const item = document.createElement("figure");
+        item.className = "dynamic-gallery-item";
+        item.style.animationDelay = i * 3 + "s";
+
+        const picture = document.createElement("picture");
+        const img = document.createElement("img");
+        img.src =
+          "/" + data.cale_galerie + "/" + selectedImages[i].cale_relativa;
+        img.alt = selectedImages[i].alternativ || selectedImages[i].nume_imag;
+        img.title = selectedImages[i].descriere_imag;
+
+        const figcaption = document.createElement("figcaption");
+        figcaption.textContent = `${selectedImages[i].nume_imag}`;
+
+        picture.appendChild(img);
+
+        item.appendChild(picture);
+        item.appendChild(figcaption);
+
+        galleryInner.appendChild(item);
+      }
+
+      gallery.appendChild(galleryInner);
+    });
+});
